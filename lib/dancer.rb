fbf127560f69a3945ce0adcf55ab3915cb1aed2e
github username <github email address>
2019-02-24 15:26:16 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+
+  attr_reader :name
+
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+def initialize(name)
+  @name = name
+end
+
+
+
+
+  end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+
+  attr_reader :name
+
+
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+
+  def initialize(name)
+    @name = name
+  end
+
+
+
+
+end
