6e0386a2ce992834ad8c7c03ddef3a9832251ba2
CalieR <calielrushton@gmail.com>
2019-02-28 14:02:18 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,8 @@
+module MetaDancing
+
+  #nothing in this file explains whether the method is intended to be used on an instance or on a class...
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+
+  # NameSpacing (::) refers to the parnt and child relationship of nested modules 
+  extend FancyDance::ClassMethods
+  # with extend, a class can use all the methods of a module as class methods
+  include FancyDance::InstanceMethods
+  # with include, a class can use all the methods of a module as instance methods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
